55cb067174904e5b9244c26ff7ba23b65e435791
LAPTOP SHOP <aminahamed672000@gmail.com>
2023-07-12 19:23:40 +0300

    first commit

--- lib/home/hadeth/hadeth_tab.dart
+++ lib/home/hadeth/hadeth_tab.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'hadeth_model.dart';
 import 'hadeth_widget.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
